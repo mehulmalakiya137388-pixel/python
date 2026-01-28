4c0572667d093235f082c7996cfb3da485bbc80a
mehulmalakiya137388-pixel <mehul.malakiya137388@marwadiuniversity.ac.in>
2026-01-28 15:41:35 +0530

    Add files via upload

---
+++ Arithmetic.py
@@ -0,0 +1,10 @@
+a=int(input("Enter your First number:"))
+b=int(input("Enter your Second number:"))
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+print("Addition",add)
+print("Subtraction",sub)
+print("Multipliction",mul)
+print("Division",div)
