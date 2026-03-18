74772c412c268617b9126fc1dc629cd3fcf75d56
Mehul Malakiya <mehul.malakiya137388@marwadiuniversity.ac.in>
2026-03-18 15:12:38 +0530

    Time: 2279 ms (28.82%) | Memory: 27.6 MB (5.16%) - LeetSync

---
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.py
@@ -0,0 +1,56 @@
+class Solution(object):
+    def findSubstring(self, s, words):
+        """
+        :type s: str
+        :type words: List[str]
+        :rtype: List[int]
+        """
+    
+        ret = []
+        n = len(s)
+        k = len(words)
+        m = len(words[0])
+        p = m * k 
+        initWindow = []
+        notValid = set()
+        valid = set()
+
+        if n < p:
+            return []
+            
+        def validSubtring(substring):
+            tempWords = words[:]
+            i = 0
+            while i < p:
+                currWord = substring[i: i + m]
+                if currWord in tempWords:
+                    tempWords.remove(currWord)
+                i += m
+            
+            return tempWords == []
+
+        for i in range(p):
+            letter = s[i]
+            initWindow.append(letter)
+
+        if validSubtring("".join(initWindow)):
+            ret.append(0)
+
+        i = p
+        
+        while i < n:
+            letter = s[i]
+            initWindow.pop(0)
+            initWindow.append(letter)
+            tempString = "".join(initWindow)
+           
+            if tempString not in notValid:
+                if tempString in valid or validSubtring(tempString):
+                    ret.append(i - p + 1)
+                    valid.add(tempString)
+                else:
+                    notValid.add(tempString)
+
+            i += 1
+        
+        return ret
